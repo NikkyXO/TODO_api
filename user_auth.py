13c3703310a1bcc914099adfd538f045153aa369
NikkyXO <alexisdame2017@gmail.com>
2023-02-06 17:19:14 +0100

    updated auth.py

--- user_auth.py
+++ user_auth.py
@@ -5,60 +5,77 @@ from flask_api import status
 from flask_jwt_extended import (
     create_access_token, jwt_required
 )
-from flask import jsonify, request
+from flask import jsonify, request, abort
 
 db = SessionLocal()
 
 
 def register():
-    """
-    It creates a new user
+    """ It creates a new user
     :return: A response object
     """
-    try:
-        username = request.form["username"]
-        password = request.form["password"]
-        email = request.form["email"]
 
+    input_data = None
+    error_msg = None
+    try:
+        input_data = request.form
+    except Exception as e:
+        input_data = None
+
+    if input_data is None:
+        error_msg = "Wrong format"
+
+    if error_msg is None and input_data.get("username"):
         check_username_exist = db.query(User).filter(
-            User.username == username).first()
-        check_email_exist = db.query(User).filter(User.email == email).first()
+            User.username == input_data.get("username")).first()
 
         if check_username_exist:
-            return {"message": f"Account with username {username} already exist"}, status.HTTP_400_BAD_REQUEST
+            error_msg = "username already exists"
+
+    if error_msg is None and input_data.get("email"):
+        check_email_exist = db.query(User).filter(User.email == input_data.get("email")).first()
 
         if check_email_exist:
-            return {"message": "Email  already taken"}, status.HTTP_400_BAD_REQUEST
+            error_msg = "email already exists"
 
-        new_user = User(username=username, email=email, password=password)
-        new_user.hash_password()
-        db.add(new_user)
-        db.commit()
-        db.refresh(new_user)
+    if error_msg is None:
+        try:
+            user = User()
+            user.email = input_data.get("email")
+            user.password = input_data.get("password")
+            user.username = input_data.get("username")
 
-        # Instantiates a new profile object here
-        profile_obj = Profile(
-            user_id=new_user.id,
-            user=new_user)
+            # user.save()
+            db.add(user)
+            db.commit()
+            db.refresh(user)
 
-        db.add(profile_obj)
-        db.commit()
-        return {"message": "User Created Successfully."}, status.HTTP_201_CREATED
+            # Instantiates a new profile object here
+            user_profile = Profile()
+            user_profile.user_id = user.id
+            user_profile.user = user
+            db.add(user_profile)
+            db.commit()
 
-    except IntegrityError as e:
-        db.rollback()
-        return jsonify(message=str(e)), status.HTTP_400_BAD_REQUEST
+            db.refresh(user)
+
+            return jsonify(user.to_json()), status.HTTP_201_CREATED
+        except Exception as e:
+            error_msg = "Can't create User: {}".format(e)
+    return jsonify({'error': error_msg}), status.HTTP_400_BAD_REQUEST
 
 
 def login():
-    """
-    It takes in a request and input data, validates the input data
+    """ It takes in a request and input data, validates the input data
     the password is correct, and returns  an access token
     :request body input_data: The data that is passed to the function
     :return: A dictionary with the keys: access token,  message, status
     """
-    username = request.form["username"]
-    password = request.form["password"]
+
+    input_data = request.form
+    username = input_data.get("username")
+    password = input_data.get("password")
+
     try:
         get_user = db.query(User).filter(User.username == username).first()
         if not get_user:
@@ -77,21 +94,8 @@ def login():
 
 
 @jwt_required()
-# def logout():
-#     """
-#         It logs out a user
-#         :return: A response object
-#     """
-#     jti = get_jwt_identity()
-#     jwt.revoke_token(jti)
-#     return jsonify({"msg": "Successfully logged out"}), 200
-
-# to implement : delete account
-
-@jwt_required()
 def delete_account(email, username):
-    """
-        It takes parameters and deletes a user account
+    """ It takes parameters and deletes a user account
         :param email: The email of the user
         :param username: The name of the user
         :return: A Response Object.
